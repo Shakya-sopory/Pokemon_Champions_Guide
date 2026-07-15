bc2a3d37b4c7e11c54c132157d5bc56696f5c4d0
Shakya Sopory <shakyasopory@gmail.com>
2026-07-15 16:46:00 +0530

    initialized backend for homepage

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from backend.parser import get_pkmn
+from backend.parser import get_Usage
+from backend.parser import get_usage_ranking
+from backend.parser import get_all_pokemon
 app=FastAPI()
 @app.get("/")
 def home():
     return {
         "message": "Pokemon_Guide website is working"
     }
+@app.get("/pokemon/{name}")
+def pokemon(name:str):
+    pokemon=get_pkmn(name)
+    if pokemon is None:
+        return {
+            "Error": "Pokemon not found"
+        }
+    return pokemon
+@app.get("/{name}/Usage")
+def Usage(name: str):
+    Usage_rate=get_Usage(name)
+    return Usage_rate
+@app.get("/home")
+def top_20_usage():
+    return get_usage_ranking()
+@app.get("/Pokemon")
+def get_all():
+    return get_all_pokemon()
--- backend/parser.py
+++ backend/parser.py
@@ -3,6 +3,10 @@ with open("data/gen9championsvgc2026regmb-1760.json","r",encoding="utf-8"
           )as f: 
     data=json.load(f)
 pkmn_database=data["data"]
+tot_pkmn=len(pkmn_database) 
+def get_all_pokemon():
+    return list(pkmn_database.keys())
+        
 def get_pkmn(name):
     if name not in pkmn_database:
         return None
@@ -16,6 +20,20 @@ def get_pkmn(name):
         "spreads": pkmn["Spreads"],
         "team-mates": pkmn["Teammates"],
         "checks and Counters": pkmn["Checks and Counters"] 
+    }
+def get_usage_ranking():
+    ranking=[]
+    for name in pkmn_database:
+        ranking.append({
+            "name": name,
+            "usage": pkmn_database[name]["usage"]
+        })
+    ranking=sorted(ranking,key=lambda pokemon:pokemon["usage"],reverse=True)
+    return ranking[:20]
+def get_Usage(name):
+    return {
+        "name": name,
+        "usage": pkmn_database[name]["usage"]
     }
 def get_category(name, category):
     return pkmn_database[name][category]
